8f5b207c6bc4b342290417fabb1b9ccd1bcbd17e
wardenger <wardenger@gmail.com>
2017-08-24 10:24:18 +0800

    update

--- controllers/Admin.js
+++ controllers/Admin.js
@@ -583,7 +583,7 @@ class Admin {
         })
         return item
       })
-      
+
       result = result.map((item, index) => {
         item.timelines = item.timelines.sort((a, b) => {
           return new Date(a.line_time).getTime() - new Date(b.line_time).getTime()
@@ -598,8 +598,16 @@ class Admin {
 
   static async getDevice(ctx) {
     try {
+      
       const deviceId = ctx.deviceId
       const doc = await Device.findById({_id: deviceId })
+
+      doc = doc.map((item, index) => {
+        item.timelines = item.timelines.sort((a, b) => {
+          return new Date(a.line_time).getTime() - new Date(b.line_time).getTime()
+        })
+      })
+
       ctx.body = { code: 200, message: '获取成功', data: doc }
     } catch (e) {
       logger.error('admin get device error', e)
