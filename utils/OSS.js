bbd3aa2a4283cb5fb200553295713032ebeaaa8a
Cbinbin <1142293069@qq.com>
2017-10-02 10:39:36 +0800

    change oss

--- utils/OSS.js
+++ utils/OSS.js
@@ -30,11 +30,15 @@ class OSS {
         co(function* () {
           var result = yield client.put(`${single}/${fileName}`, ctx.req.file.path)
           if(result.res && result.res.statusCode === 200) {
-            var signUrl = client.signatureUrl(`${single}/${fileName}`, {process: 'image/resize,p_80/quality,Q_80'})
-            resolve ({
-              signUrl: signUrl,
-              pubUrl: result.url,
-            })
+            var signUrl = client.signatureUrl(`${single}/${fileName}`, {expires: 28800, process: 'image/resize,p_80/quality,Q_80'})
+            var pubResult = yield client.get(`${single}/${fileName}`, `./static/example-cascade.jpg`, 
+                                {process: 'image/resize,p_80/quality,Q_80'})
+            if(pubResult.res  && pubResult.res.statusCode === 200) {
+              resolve ({
+                signUrl: signUrl,
+                pubUrl: pubResult.res.requestUrls[0],
+              })
+            }
           } else resolve(result)
         }).catch( (err)=> {
           console.log(err)
@@ -51,14 +55,69 @@ class OSS {
       uploads(ctx.req, ctx.res, (err)=> {
         if(err) return reject(JSON.stringify(err))
         var urlArr = []
-        if(ctx.req.files) {
+        if(ctx.req.files[0]) {
+          ctx.req.files.forEach((fileItem, index)=> {
+            setTimeout(()=> {
+              co(function* () {
+                var fileName = Date.now() + '_' + fileItem.originalname
+                var result = yield client.put(`${single}/${fileName}`, fileItem.path)
+                if(result.res && result.res.statusCode === 200) {
+                  var pubResult = yield client.get(`${single}/${fileName}`, `./static/example-cascade.jpg`, 
+                                  {process: 'image/resize,p_80/quality,Q_80'})
+                  if(pubResult.res && pubResult.res.statusCode === 200) urlArr.push(pubResult.res.requestUrls[0])
+                  if(index == (ctx.req.files.length - 1) ) resolve(urlArr)
+                }
+              }).catch( (err)=> {
+                console.log(err)
+              })
+            }, 500*index)
+          })
+        } else resolve(null)
+      })
+    })
+    return uploadsmul
+  }
+
+  uploadMulterSign(single, ctx) {
+    const client = this.client
+    var upload = multer({ storage: multer.diskStorage({}) }).single(single)
+    var uploadmul = new Promise((resolve, reject)=> {
+      upload(ctx.req, ctx.res, (err)=> {
+        if(err) return reject(JSON.stringify(err))
+        // if(ctx.req.file.fieldname !== single) return resolve({key: ctx.req.file.fieldname})
+        var fileName = Date.now() + '_' + ctx.req.file.originalname
+        co(function* () {
+          var result = yield client.put(`${single}/${fileName}`, ctx.req.file.path)
+          if(result.res && result.res.statusCode === 200) {
+            var signUrl = client.signatureUrl(`${single}/${fileName}`, {expires: 28800, process: 'image/resize,p_80/quality,Q_80'})
+            resolve ({
+              signUrl: signUrl,
+              pubUrl: result.url,
+            })
+          } else resolve(result)
+        }).catch( (err)=> {
+          console.log(err)
+        })
+      })
+    })
+    return uploadmul
+  }
+
+  uploadMultersSign(single, ctx) {
+    const client = this.client
+    var uploads = multer({ storage: multer.diskStorage({}) }).array(single, 10)
+    var uploadsmul = new Promise((resolve, reject)=> {
+      uploads(ctx.req, ctx.res, (err)=> {
+        if(err) return reject(JSON.stringify(err))
+        var urlArr = []
+        if(ctx.req.files[0]) {
           ctx.req.files.map((fileItem, index)=> {
             co(function* () {
               // if(ctx.req.file.fieldname !== single) return resolve({key: ctx.req.file.fieldname})
               var fileName = Date.now() + '_' + fileItem.originalname
               var result = yield client.put(`${single}/${fileName}`, fileItem.path)
               if(result.res && result.res.statusCode === 200) {
-                var signUrl = client.signatureUrl(`${single}/${fileName}`, {process: 'image/resize,p_80/quality,Q_80'})
+                var signUrl = client.signatureUrl(`${single}/${fileName}`, {expires: 28800, process: 'image/resize,p_80/quality,Q_80'})
                 // console.log ({ signUrl: signUrl, pubUrl: result.url, })
                 urlArr.push(signUrl)
                 if(index == (ctx.req.files.length - 1) ) resolve(urlArr)
