65da519f470642dedb67a0891c8932b3d4415ed8
Cbinbin <1142293069@qq.com>
2017-10-10 18:08:07 +0800

    chnage

--- tcp/transform_data.js
+++ tcp/transform_data.js
@@ -4,29 +4,34 @@ const crypto = require('crypto')
 
 const logger = require('../utils/logger')
 
-module.exports = (data) => {
+module.exports = (data, conn) => {
 
-  data = new Buffer(data, 'base64')
-  const decipher = crypto.createDecipher('aes-128-ecb', salt)
-  decipher.setAutoPadding(false)
-  let decrypted = decipher.update(data, 'binary', 'utf8')
-  decrypted += decipher.final('utf8')
+  try {
+    data = new Buffer(data, 'base64')
+    const decipher = crypto.createDecipher('aes-128-ecb', salt)
+    decipher.setAutoPadding(false)
+    let decrypted = decipher.update(data, 'binary', 'utf8')
+    decrypted += decipher.final('utf8')
 
-  decrypted = decrypted.replace(/\0/g, '')
-  console.log(decrypted)
-  const obj = JSON.parse(decrypted)
-  console.log(obj)
+    decrypted = decrypted.replace(/\0/g, '')
+    console.log(decrypted)
+    const obj = JSON.parse(decrypted)
+    console.log(obj)
 
-  let new_indexes = []
-  obj.indexes.forEach((item)=> {
-    if(quotaDic(Object.keys(item)[0]).quotaName != '未知') {
-      new_indexes.push(item)
+    let new_indexes = []
+    obj.indexes.forEach((item)=> {
+      if(quotaDic(Object.keys(item)[0]).quotaName != '未知') {
+        new_indexes.push(item)
+      }
+    })
+
+    return {
+      number: obj.deviceId,
+      data: new_indexes, //obj.indexes,
+      ts: obj.ts
     }
-  })
-
-  return {
-    number: obj.deviceId,
-    data: new_indexes, //obj.indexes,
-    ts: obj.ts
+  } catch(e) {
+    conn.write('PEE')
+    console.error(e)
   }
 }
--- tcpSocket.js
+++ tcpSocket.js
@@ -9,6 +9,7 @@ const deviceId_find_db = require('./tcp/deviceId_find_db')
 const save_to_db = require('./tcp/save_to_db')
 const to_app_data = require('./tcp/to_app_data')
 const myEmitter = require('./tcp/emitter')
+const redis = require('./redis_tcp')
 
 
 const server = net.createServer()
@@ -34,20 +35,24 @@ function handleConnection(conn) {
     // logger.info('DRM_DATA:', d)
     // logger.info('DRM_DATA:', transform_data(d))
 
+    const nowtime_stamp = new Date().getTime().toString()
     const dataSource = verify_source(d)
 
     if(dataSource === 'hardware') {
       try {
-        const normal_data = transform_data(d)
+        const normal_data = transform_data(d, conn)
         console.log(normal_data)
+
+        redis.set(`drm_${nowtime_stamp}`, JSON.stringify(normal_data))
+        myEmitter.emit('coming', to_app_data(normal_data))
         const device_data = await deviceId_find_db(normal_data)
         if(device_data) {
-
-          // setTimeout(() => {
-          const now_data = normal_data
-          save_to_db(now_data)
-          myEmitter.emit('coming', to_app_data(now_data))
-          // }, 1000*1 )
+          setTimeout(() => {
+            redis.get(`drm_${nowtime_stamp}`, (err, replie)=> {
+              if(err) return console.log('redisErr:' + err)
+              save_to_db(JSON.parse(replie))
+            })
+          }, 1000*3 )
         } else {
           logger.info(`save failed: Didn't found this deviceId`)
           console.log(`Didn't found this deviceId`)
