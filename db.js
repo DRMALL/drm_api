354f3b7652e4a80229a7bf50ba76fcfbe6b1fa3c
joephon <306519779@qq.com>
2017-11-26 18:06:26 +0800

    added

--- db.js
+++ db.js
@@ -1,9 +1,13 @@
+require('dotenv').config()
+const DATABASE_USER = process.env.DATABASE_USER
+const DATABASE_PWD = process.env.DATABASE_PWD
+
 const mongoose = require('mongoose')
   , db = mongoose.connection
   
 mongoose.Promise = global.Promise
 
-mongoose.connect('mongodb://localhost/drm', {useMongoClient: true})
+mongoose.connect(`mongodb://localhost/drm?user=${DATABASE_USER}&pass=${DATABASE_PWD}`, {useMongoClient: true})
 
 db.on('error', console.error.bind(console, 'connect error:'))
 
